f0a9b5859376680f2bf262699029cd0e3a14c317
Catborisovv <69162936+DEV-Catborisovv@users.noreply.github.com>
2023-03-12 03:45:14 +0300

    delete linux-compile

--- sampgo.go
+++ sampgo.go
@@ -5,10 +5,6 @@ package sampgo
 #cgo windows CFLAGS: -DHAVE_INTTYPES_H -DHAVE_MALLOC_H -DHAVE_STDINT_H -DWIN32
 #cgo windows LDFLAGS: "-Wl,--subsystem,windows,--kill-at"
 
-#cgo linux CFLAGS: -I./lib -I./lib/amx -Wno-attributes -Wno-implicit-function-declaration
-#cgo linux CFLAGS: -DHAVE_INTTYPES_H -DHAVE_MALLOC_H -DHAVE_STDINT_H -DLINUX -D_GNU_SOURCE
-#cgo linux LDFLAGS: -ldl
-
 #ifndef GOLANG_APP
 #define GOLANG_APP
 
